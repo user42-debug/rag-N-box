95c98d723eb8179b845ee7c6eb045b9c04ef0284
martin <mdescombes21540@gmail.com>
2026-03-25 09:50:57 +0100

    Fixed some bugs

--- sources/messages.py
+++ sources/messages.py
@@ -2,27 +2,22 @@
 import time
 import serial
 
-SERIAL_PORT = "COM5"
-BAUD = 9600 
-
-ser = serial.Serial(SERIAL_PORT, BAUD, timeout=1)
-
 class Message:
     def __init__(self, port):
         self.SERIAL_PORT = port
         self.BAUD = 9600
-        self.ser = serial.Serial(SERIAL_PORT, BAUD, timeout=1)
+        self.ser = serial.Serial(self.SERIAL_PORT, self.BAUD, timeout=1)
 
     def send_at(self, cmd, wait=0.5):
         self.ser.write((cmd + "\r\n").encode())
         time.sleep(wait)
-        resp = ser.read_all().decode(errors="ignore")
+        resp = self.ser.read_all().decode(errors="ignore")
         print(f">>> {cmd}")
         print(resp.strip())
         return resp
 
 
-    def send_message(self, numero,message):
+    def send_message(self, numero, message):
         
         time.sleep(0.1)
         self.send_at("AT")
@@ -31,6 +26,6 @@ class Message:
         time.sleep(0.1)
         self.send_at(f'AT+CMGS="{numero}"')
         time.sleep(0.1)
-        self.ser.write(message.encode()
+        self.ser.write(message.encode())
         time.sleep(0.1)
         self.send_at("\x1A")
